88662133001775d37bc191f3a948032882695827
Yngrid Alves <49573759+Gridialves@users.noreply.github.com>
2019-07-15 19:21:28 -0300

    Add files via upload

---
+++ bazar/bazarClothes.js
@@ -0,0 +1,72 @@
+function $_GET(param) {
+    var vars = {};
+    window.location.href.replace(location.hash, '').replace(
+        /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+        function (m, key, value) { // callback
+            vars[key] = value !== undefined ? value : '';
+        }
+    );
+
+    if (param) {
+        return vars[param] ? vars[param] : null;
+    }
+    return vars;
+}
+
+let id = $_GET("id");
+
+fetch(`http://localhost:3000/bazar/${id}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const container = document.querySelector(".box-clothes-det");
+        const box = document.createElement('div');
+        box.setAttribute('class', 'box-clothes-det-div-imagem');
+        box.setAttribute('data-id', data._id)
+
+        const imagem = document.createElement('img');
+        imagem.setAttribute('class', 'img-clothes');
+        imagem.setAttribute('alt', data.nome);
+        imagem.setAttribute('src', data.imagem)
+
+        const body = document.createElement('div');
+        body.setAttribute('class', 'box-clothes-det-body');
+
+        const nome = document.createElement('p');
+        nome.innerHTML = `${data.nome}`
+        nome.setAttribute('class', 'box-clothes-body-paragrafo-nome');
+
+        const valor = document.createElement('p');
+        valor.innerHTML = `R$: ${data.valor}`
+        valor.setAttribute('class', 'box-clothes-body-paragrafo-valor');
+
+        const addCart = document.createElement('button');
+        addCart.innerHTML = `Adicionar ao carrinho`
+        addCart.setAttribute('class', 'box-clothes-det-btn');
+
+        addCart.addEventListener('click', () => {
+            //pega a sessao pelo cookie do carrinho e adiciona o id do produto
+            fetch(`http://localhost:3000/cart`)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    return data;
+                })
+
+        })
+
+        container.appendChild(box);
+        container.appendChild(body);
+        box.appendChild(imagem);
+        body.appendChild(nome);
+        body.appendChild(valor);
+        body.appendChild(addCart);
+
+    })
+    .catch(erro => {
+        console.log("Deu erro!!!", erro)
+    })
+
+
---
+++ bazar/shoppingCart.js
@@ -0,0 +1,78 @@
+
+fetch('http://localhost:3000/shopcart')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        const container2 = document.querySelector(".shopping-cart");
+        data.forEach(shopCart => {
+
+            const box = document.createElement('div');
+            box.setAttribute('class', 'item');
+
+            const button = document.createElement('div');
+            button.setAttribute('class', 'buttons');
+
+            const imageButton = document.createElement('img');
+            imageButton.setAttribute('class', 'delete-btn');
+            imageButton.setAttribute('src', "./assets/delete.png");
+
+            const boxImage = document.createElement('div');
+            boxImage.setAttribute('data-id', shopCart._id)
+
+            const imagem = document.createElement('img');
+            imagem.setAttribute('class', 'image');
+            imagem.setAttribute('alt', shopCart.nome);
+            imagem.setAttribute('src', shopCart.imagem);
+
+            const description = document.createElement('div');
+            description.setAttribute('class', 'description');
+
+            const quantity = document.createElement('div');
+            quantity.setAttribute('class', 'quantity');
+
+            const quantityInput = document.createElement('input');
+            quantityInput.setAttribute('value', '1');
+
+            const valor = document.createElement('div');
+            valor.innerHTML = `R$: ${shopCart.valor}`
+            valor.setAttribute('class', 'total-price');
+
+
+            box.appendChild(button);
+            box.appendChild(boxImage);
+            box.appendChild(description);
+            box.appendChild(quantity);
+            box.appendChild(valor);
+            button.appendChild(imageButton);
+            boxImage.appendChild(imagem);
+            quantity.appendChild(quantityInput);
+            container2.appendChild(box);
+
+            imageButton.addEventListener('click', () => {
+                fetch(`http://localhost:3000/bazar/${shopCart._id}`, {
+                    method: 'PUT',
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ cart: false })
+                })
+
+                    .then(() => {
+                        window.location.reload()
+
+                    })
+
+                    .catch(erro => {
+                        console.log(erro)
+                    })
+            })
+        });
+    })
+    .catch(erro => {
+        console.log("Deu erro!!!", erro)
+    });
+
+
+
